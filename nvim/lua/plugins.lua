b40f82a0a2944b1c9e073227254e3a0763a5ef2c
Gilberto Leon <ripflame@gmail.com>
2025-04-05 10:06:40 -0600

    Changing contrast mode for theme to medium

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -9,7 +9,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.g.everforest_background = 'hard'
+      vim.g.everforest_background = 'medium'
       vim.g.everforest_enable_italic = true
       vim.cmd.colorscheme('everforest')
     end
