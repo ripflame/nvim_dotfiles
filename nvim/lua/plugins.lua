2cd748eb9d8de23104bd2fc522d1cad08b1bdaa7
Gilberto Leon <ripflame@gmail.com>
2025-05-03 20:43:04 -0600

    Making luasnip auto load snippets when added

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -146,7 +146,11 @@ return {
     "L3MON4D3/LuaSnip",
     build = "make install_jsregexp",
     config = function()
-      require("luasnip.loaders.from_snipmate").lazy_load()
+      local snippets_path = vim.fn.expand("~/.nvim-setup/nvim/snippets")
+      require("luasnip.loaders.from_snipmate").lazy_load({
+        paths = snippets_path,
+        fs_event_providers = { "libuv" }
+      })
     end,
   },
 
