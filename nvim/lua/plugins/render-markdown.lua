7d6bda6b1ede18ace82b19cb9032ff657b15cad2
Gilberto Leon <ripflame@gmail.com>
2025-09-18 15:48:09 -0600

    removing duplicate options in render-markdown config

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -1,26 +1,21 @@
 return {
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "echasnovski/mini.nvim"
-    },
-    opts = {
-      code = {
-        language_border = ' ',
-        highlight = 'RenderMarkdownCode',
-      }
-    },
-    config = function()
-      require('render-markdown').setup({
-        code = {
-          language_border = ' ',
-          highlight = 'RenderMarkdownCode',
-        }
-      })
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"echasnovski/mini.nvim",
+		},
+		config = function()
+			require("render-markdown").setup({
+				code = {
+					language_border = " ",
+					highlight = "RenderMarkdownCode",
+				},
+				render_modes = true,
+			})
 
-      -- Set custom background color for code blocks (Everforest medium bg1)
-      vim.api.nvim_set_hl(0, 'RenderMarkdownCode', { bg = '#343F44' })
-    end,
-  }
+			-- Set custom background color for code blocks (Everforest medium bg1)
+			vim.api.nvim_set_hl(0, "RenderMarkdownCode", { bg = "#343F44" })
+		end,
+	},
 }
