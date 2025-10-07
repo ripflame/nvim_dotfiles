4ceb350b7ddb3645753dbb2102924cda7e430a4d
Gilberto Leon <ripflame@gmail.com>
2025-10-07 10:27:43 -0600

    switching to rose pine theme and blending telescope

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,32 @@
 return {
   -- Everforest colorscheme
+  -- {
+  --   'sainnhe/everforest',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.g.everforest_background = 'medium'
+  --     vim.g.everforest_enable_italic = true
+  --     vim.cmd.colorscheme('everforest')
+  --   end
+  -- },
+
+  -- lua/plugins/rose-pine.lua
   {
-    'sainnhe/everforest',
-    lazy = false,
-    priority = 1000,
+    "rose-pine/neovim",
+    name = "rose-pine",
     config = function()
-      vim.g.everforest_background = 'medium'
-      vim.g.everforest_enable_italic = true
-      vim.cmd.colorscheme('everforest')
+      require("rose-pine").setup({
+        highlight_groups = {
+          TelescopeBorder = { fg = "highlight_high", bg = "none" },
+          TelescopeNormal = { bg = "none" },
+          TelescopePromptNormal = { bg = "base" },
+          TelescopeResultsNormal = { fg = "subtle", bg = "none" },
+          TelescopeSelection = { fg = "text", bg = "base" },
+          TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
+        },
+      })
+      vim.cmd("colorscheme rose-pine")
     end
   },
   
