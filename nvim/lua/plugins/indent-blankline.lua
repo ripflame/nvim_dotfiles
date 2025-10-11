22f07bb76d1814fc13d023999aa7e98660ce330b
Gilberto Leon <ripflame@gmail.com>
2025-10-11 11:47:14 -0600

    matching indent-blankline colors to rose pine theme

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -15,13 +15,13 @@ return {
 
     local hooks = require "ibl.hooks"
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-      vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-      vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-      vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+      vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#eb6f92" })      -- love
+      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#f6c177" })   -- gold
+      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#31748f" })     -- pine
+      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#ebbcba" })   -- rose
+      vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#9ccfd8" })    -- foam
+      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#c4a7e7" })   -- iris
+      vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#9ccfd8" })     -- foam (cyan)
     end)
 
     require("ibl").setup {
