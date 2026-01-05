19cc3cc22a6bef578c08471ddecb2d0c240e2918
Gilberto Leon <ripflame@gmail.com>
2026-01-05 12:55:30 -0600

    Updating treesitter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,30 +1,14 @@
 return {
-  -- Main treesitter plugin
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
     config = function()
-      require 'nvim-treesitter.configs'.setup {
+      require("nvim-treesitter").setup({
         ensure_installed = { "markdown", "javascript", "typescript", "lua", "python", "html", "css" },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-        matchup = { enable = true },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "gnn",
-            node_incremental = "grn",
-            scope_incremental = "grc",
-            node_decremental = "grm",
-          },
-        },
-      }
-    end
+      })
+    end,
   },
-
-  -- Treesitter context
   {
     "nvim-treesitter/nvim-treesitter-context",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
@@ -35,6 +19,6 @@ return {
       min_window_height = 1,
       mode = "cursor",
       separator = "─",
-    }
+    },
   },
 }
